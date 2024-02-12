14e9cc341a22a389cb24e3117fbe534db2aceff5
mushi333 <mujtaba.zahidi24@gmail.com>
2024-02-12 18:29:36 +1100

    Skeleton commit with unique font family

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Stardew Valley Fishing Simulator</h1>
+      <p>Happy Valentines dear Sarah. I have made this game just for you!</p>
+    </div>
+  );
+}
+
+export default App;
