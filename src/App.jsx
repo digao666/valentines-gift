dfa036d1358afd2ba0632ade1b071f45f1ed0037
mushi333 <mujtaba.zahidi24@gmail.com>
2024-02-12 18:39:56 +1100

    Added layout for the game box

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Stardew Valley Fishing Simulator</h1>
-      <p>Happy Valentines dear Sarah. I have made this game just for you!</p>
-    </div>
+    <>
+      <div className="title">
+        <h1>Stardew Valley Fishing Simulator</h1>
+        <p>Happy Valentines dear Sarah. I have made this game just for you!</p>
+      </div>
+      <div className="game-box">
+        <div className="game"></div>
+      </div>
+    </>
   );
 }
 
