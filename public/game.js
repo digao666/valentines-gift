cbe2dd5ed01a28dfa4f8ff2b5de9387ac31f89ac
mushi333 <mujtaba.zahidi24@gmail.com>
2024-02-13 15:45:24 +1100

    Added basic game mechanic

--- public/game.js
+++ public/game.js
@@ -2,13 +2,6 @@ let config = {
   type: Phaser.AUTO,
   width: 1000,
   height: 800,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 300 },
-      debug: false,
-    },
-  },
   scene: {
     preload: preload,
     create: create,
@@ -16,19 +9,33 @@ let config = {
   },
 };
 
-const MAX_FISH_BAR_HEIGH = 0;
-const MIN_FISH_BAR_HEIGHT = (347, 570);
-const MAX_FISH_BAR_HEIGHT = 0;
-const MAX_ROD_BAR_HEIGHT = 0;
-const MIN_ROD_BAR_HEIGHT = (397, 612);
-const MAX_FISH_HEIGHT = 0;
-const MIN_FISH_HEIGHT = (347, 570);
+const FISHING_ROD_BOTTOM_X = 347;
+const FISHING_ROD_TOP_Y = 100;
+const FISHING_ROD_BAR_BOTTOM_Y = 570;
+const MAX_FISH_BAR_COORDINATES = [FISHING_ROD_BOTTOM_X, FISHING_ROD_TOP_Y];
+const MIN_FISH_BAR_COORDINATES = [
+  FISHING_ROD_BOTTOM_X,
+  FISHING_ROD_BAR_BOTTOM_Y,
+];
+const MIN_ROD_BAR_COORDINATES = [397, 623];
+const MAX_FISH_COORDINATES = [FISHING_ROD_BOTTOM_X, FISHING_ROD_TOP_Y];
+const MIN_FISH_COORDINATES = [FISHING_ROD_BOTTOM_X, FISHING_ROD_BAR_BOTTOM_Y];
+const MAX_FISHING_BAR_VALUE = 100;
+const MIN_FISHING_BAR_VALUE = 5;
+const SPEED = 2;
+const X = 0;
+const Y = 1;
 
 let player;
 let fish;
 let message;
 let fishBar;
-let fishingRodBar;
+let fishingRod;
+let rodBar;
+let background;
+let caughtBackground;
+let isMousePressed = false;
+let fishingBarValue = MIN_FISHING_BAR_VALUE;
 
 let game = new Phaser.Game(config);
 
@@ -36,62 +43,81 @@ function preload() {
   this.load.image("background", "assets/background01.png");
   this.load.image("caughtBackground", "assets/background02.png");
   this.load.image("fishBar", "assets/bar01.png");
-  this.load.image("rodBar", "assets/bar02.png");
   this.load.image("caught", "assets/caught01.png");
   this.load.image("fish", "assets/fish01.png");
   this.load.image("fishing", "assets/fishing01.png");
   this.load.image("hit", "assets/hit01.png");
+  this.load.image("rodForeground", "assets/rodForeground01.png");
 }
 
 function create() {
-  // Background image with the "hit animation"
-  this.add.image(500, 400, "background");
-
-  let hitImage = this.add.image(525, 430, "hit").setVisible(false);
-  this.time.delayedCall(
-    1000,
-    function () {
-      hitImage.setVisible(true);
-    },
-    [],
-    this
+  background = this.add.image(500, 400, "background");
+  fishingRod = this.add.image(385, 335, "fishing");
+  rodBar = this.add
+    .image(
+      MIN_ROD_BAR_COORDINATES[X],
+      MIN_ROD_BAR_COORDINATES[Y],
+      "rodForeground"
+    )
+    .setOrigin(0.5, 1)
+    .setScale(1, fishingBarValue / MAX_FISHING_BAR_VALUE);
+  fishBar = this.add.image(
+    MIN_FISH_BAR_COORDINATES[X],
+    MIN_FISH_BAR_COORDINATES[Y],
+    "fishBar"
   );
-
-  let fishingImage = this.add.image(385, 335, "fishing").setVisible(false);
-  let rodBarImage = this.add.image(397, 612, "rodBar").setVisible(false);
-  let fishBarImage = this.add.image(347, 570, "fishBar").setVisible(false);
-  let fishImage = this.add.image(347, 570, "fish").setVisible(false);
-
-  this.time.delayedCall(
-    2000,
-    function () {
-      hitImage.setVisible(false);
-      fishingImage.setVisible(true);
-      fishImage.setVisible(true);
-      rodBarImage.setVisible(true);
-      fishBarImage.setVisible(true);
-    },
-    [],
-    this
+  fish = this.add.image(
+    MIN_FISH_COORDINATES[X],
+    Math.random() * (MIN_FISH_COORDINATES[Y] - MAX_FISH_COORDINATES[Y] + 1),
+    "fish"
   );
-
-  let caughtBackgroundImage = this.add
+  caughtBackground = this.add
     .image(500, 400, "caughtBackground")
     .setVisible(false);
-  let caughtImage = this.add.image(492, 275, "caught").setVisible(false);
-  this.time.delayedCall(
-    3000,
-    function () {
-      fishingImage.setVisible(false);
-      fishImage.setVisible(false);
-      rodBarImage.setVisible(false);
-      fishBarImage.setVisible(false);
-      caughtBackgroundImage.setVisible(true);
-      caughtImage.setVisible(true);
-    },
-    [],
-    this
-  );
+  message = this.add.image(492, 275, "caught").setVisible(false);
+
+  // Allow interactivity with the background
+  background.setInteractive();
+  background.on("pointerdown", function () {
+    isMousePressed = true;
+  });
+  background.on("pointerup", function () {
+    isMousePressed = false;
+  });
 }
 
-function update() {}
+function update() {
+  // Move the fish
+  var direction = Phaser.Math.Between(0, 1);
+
+  if (direction === 0 && fish.y > MAX_FISH_COORDINATES[Y]) {
+    fish.y -= SPEED;
+  } else if (fish.y < MIN_FISH_COORDINATES[Y]) {
+    fish.y += SPEED;
+  }
+
+  // Move the fish bar
+  if (isMousePressed && fishBar.y > MAX_FISH_BAR_COORDINATES[Y]) {
+    fishBar.y -= SPEED;
+  } else if (fishBar.y < MIN_FISH_BAR_COORDINATES[Y]) {
+    fishBar.y += SPEED;
+  }
+
+  // Adjust the fishing bar
+  if (fish.y > fishBar.y - 48 && fish.y < fishBar.y + 48) {
+    fishingBarValue += 0.1;
+  } else if (fishingBarValue > MIN_FISHING_BAR_VALUE) {
+    fishingBarValue -= 0.1;
+  }
+  rodBar.setScale(1, fishingBarValue / MAX_FISHING_BAR_VALUE);
+
+  if (fishingBarValue >= MAX_FISHING_BAR_VALUE) {
+    setTimeout(function () {}, 1000);
+    fishingRod.setVisible(false);
+    rodBar.setVisible(false);
+    fishBar.setVisible(false);
+    fish.setVisible(false);
+    caughtBackground.setVisible(true);
+    message.setVisible(true);
+  }
+}
