6f305a320d81de91a6cb95e0c0ba08f3eb8c99cd
mushi333 <mujtaba.zahidi24@gmail.com>
2024-02-13 13:21:39 +1100

    Added main images and backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         <p>Happy Valentines dear Sarah. I have made this game just for you!</p>
       </div>
       <div className="game-box">
-        <iframe width={820} height={620} src="./game.html"></iframe>
+        <iframe width={1020} height={820} src="./game.html"></iframe>
       </div>
     </>
   );
